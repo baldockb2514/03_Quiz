195263bc91295e3cf67cacdd933aa7bf0ae8269d
baldockb2514 <111828862+baldockb2514@users.noreply.github.com>
2023-06-02 10:05:27 +1200

    Add files via upload

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,40 +1,48 @@
 import random
+import numpy
+
 
 def question_generator():
-
-    functions = ["+", "-"]
-
-    first_numbers = []
+    numbers = []
     question_outline = []
 
-    for number in range(1, 2):
+    for number in range(1, 3):
 
-        number = random.randint(-10, 9)
-        if number == 0:
-            number = 10
-        first_numbers.append(number)
+        while True:
+            number = random.randint(-10, 9)
+            number_2 = -1 * number
+            if number == 0:
+                number = 10
+            if number not in numbers and number_2 not in numbers:
+                numbers.append(number)
+                break
+
         if number < 0:
-            brackets_outline = f"(x{number}"
+            brackets_outline = f"(x{number})"
         else:
-            brackets_outline = f"(x+{number}"
+            brackets_outline = f"(x+{number})"
         question_outline.append(brackets_outline)
         if len(question_outline) == 2:
             break
 
-    x_value_one = sum(first_numbers)
-    integer = first_numbers[0] * first_numbers[1]
+    x_value_one = sum(numbers)
+    integer = numpy.prod(numbers)
 
-    third_number = random.randint(-10, 9)
-    if third_number == 0:
-        third_number = 10
+    while True:
+        third_number = random.randint(-10, 9)
+        third_number_2 = -1 * third_number
+        if third_number == 0:
+            third_number = 10
+        if third_number not in numbers and third_number_2 not in numbers:
+            numbers.append(third_number)
+            break
 
     if third_number > 0:
-        third_brackets = f"(x+{third_number}"
+        third_brackets = f"(x+{third_number})"
     else:
-        third_brackets = f"(x{third_number}"
+        third_brackets = f"(x{third_number})"
 
     question_outline.append(third_brackets)
-    print(question_outline)
 
     final_x_squared_value = x_value_one + third_number
     final_x_value = third_number * x_value_one + integer
@@ -42,8 +50,27 @@ def question_generator():
 
     if final_integer > 0:
         final_integer = f"+{final_integer}"
+    if final_x_value > 0:
+        final_x_value = f"+{final_x_value}"
+    if final_x_squared_value > 0:
+        final_x_squared_value = f"+{final_x_squared_value}"
+
+    answer = f"X^3{final_x_squared_value}x^2{final_x_value}x{final_integer}"
+    print()
+    print(answer)
+    print(*question_outline)
+    get_answer = input(f"Please solve this question: ")
+    if get_answer == answer:
+        result = "correct"
+    else:
+        result = "wrong"
+    return result
 
 
+while True:
+    hard_test = question_generator()
 
-
-
+    if hard_test == "correct":
+        print("Correct! Congratulations!")
+    else:
+        print("Incorrect.")
